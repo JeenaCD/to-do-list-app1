b1fb606be2dc32ce64c4b42b6f02008cb62eed5f
JeenaCD <102554603+JeenaCD@users.noreply.github.com>
2022-05-08 09:45:35 +0530

    final

---
+++ js/validate.js
@@ -0,0 +1,17 @@
+function redirect(){
+    location.replace("../main.html");
+}
+
+function validate(callback){
+    var email=document.getElementById("email");
+    var password=document.getElementById("password");
+    if(email.value=="admin" && password.value =="12345"){
+    callback();
+        
+    }
+    else{
+        alert("Invalid user");
+        return false;
+        
+    }
+}
